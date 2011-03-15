ffae8cc8fd539ea47a82354b0e294a652ed5fcd8
Koushik Dutta <koushd@gmail.com>
2011-03-15 00:35:52 -0700

    stuff
    stuff
    
    stuff
    
    cleanups
    
    more cleanujps
    
    restructure

---
+++ AsyncTask/AsyncTask.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using async = System.Collections.IEnumerator;
+
+namespace AsyncTask
+{
+    internal class State<T>
+    {
+        public State(ManualResetEvent ev)
+        {
+            Event = ev;
+            OriginatingThread = Thread.CurrentThread;
+        }
+
+        public Exception Exception
+        {
+            get;
+            set;
+        }
+        public ManualResetEvent Event
+        {
+            get;
+            set;
+        }
+        public T Result
+        {
+            get;
+            set;
+        }
+        public Thread OriginatingThread
+        {
+            get;
+            set;
+        }
+        public AsyncTask<T> Task
+        {
+            get;
+            set;
+        }
+    }
+    
+    public class AsyncTask<T> : Task<T>
+	{
+        void Continue(Task previous)
+        {
+            if (previous != null && previous.Exception != null)
+            {
+                mState.Exception = previous.Exception;
+                mState.Event.Set();
+            }
+
+            try
+            {
+                if (!mEnumerator.MoveNext())
+                {
+                    if (previous == null)
+                        mState.Exception = new Exception("Task ended without a result.");
+                    else
+                    {
+                        Task<T> final = previous as Task<T>;
+                        if (final != null)
+                            mState.Result = final.Result;
+                        else
+                            mState.Exception = new Exception(String.Format("Task ended with a result that was not of Type {0}.", typeof(T)));
+                    }
+                    mState.Event.Set();
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                mState.Exception = ex;
+                mState.Event.Set();
+                return;
+            }
+
+            object o = mEnumerator.Current;
+            Task t = o as Task;
+            if (t != null)
+            {
+                t.ContinueWith(Continue);
+                if (mState.OriginatingThread == Thread.CurrentThread)
+                    t.Start();
+                else
+                    t.RunSynchronously();
+            }
+            else
+            {
+                mState.Result = (T)o;
+                mState.Event.Set();
+            }
+        }
+        
+        static T Do(async tasks, State<T> state)
+		{
+            state.Task.Continue(null);
+            state.Event.WaitOne();
+            if (state.Exception != null)
+                throw state.Exception;
+            return state.Result;
+		}
+
+		async mEnumerator;
+        State<T> mState;
+        AsyncTask(async tasks, State<T> state)
+            : base(delegate { return Do(tasks, state); })
+		{
+			mEnumerator = tasks;
+            mState = state;
+            mState.Task = this;
+		}
+
+        public AsyncTask(async tasks)
+            : this(tasks, new State<T>(new ManualResetEvent(false)))
+        {
+        }
+	}
+
+    public static class TaskHelper
+	{
+        public static Task<string> DownloadStringTask(this WebClient client, String url)
+        {
+            return new Task<string>(delegate
+            {
+                return client.DownloadString(url);
+            });
+        }
+
+        public static Task<T> Create<S,T>(Func<S, T> func, S s)
+        {
+            return new Task<T>(delegate { return func(s); });
+        }
+
+        public static Task<T> Create<R, S, T>(Func<R, S, T> func, R r, S s)
+        {
+            return new Task<T>(delegate { return func(r, s); });
+        }
+
+        public static Task<T> Create<Q, R, S, T>(Func<Q, R, S, T> func, Q q, R r, S s)
+        {
+            return new Task<T>(delegate { return func(q, r, s); });
+        }
+
+    }
+}
---
+++ AsyncTask/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+using async = System.Collections.IEnumerator;
+
+namespace AsyncTask
+{
+    class Foo
+    {
+        public static Foo operator ++(Foo c1)
+        {
+            return c1;
+        }
+    }
+
+	class Program
+	{
+		static async Test1()
+		{
+            // download the google home page
+			WebClient client = new WebClient();
+			var google = client.DownloadStringTask("http://www.google.com");
+			yield return google;
+            // find and download the first link on the google home page
+            Regex regex = new Regex("href=\"(?<href>http://.*?)\"");
+            var match = regex.Match(google.Result);
+            var firstHref = match.Groups["href"].Value;
+            var firstHrefTask = TaskHelper.Create(client.DownloadString, firstHref);
+            yield return firstHrefTask;
+        }
+
+        static async Test2()
+        {
+            // download the microsoft home page
+            WebClient client = new WebClient();
+            var microsoft = client.DownloadStringTask("http://www.microsoft.com");
+            yield return microsoft;
+            // find and download the first link on the microsoft home page
+            Regex regex = new Regex("href=\"(?<href>http://.*?)\"");
+            var match = regex.Match(microsoft.Result);
+            var firstHref = match.Groups["href"].Value;
+            var firstHrefTask = client.DownloadStringTask(firstHref);
+            yield return firstHrefTask;
+
+            // the return doesn't need to be a task, for example, returning an object is completely
+            // legitimate async result.
+            // return the the first link found and an excerpt:
+            yield return firstHref + ": " + firstHrefTask.Result.Substring(0, 100) + "...";
+        }
+
+
+        static async Test3()
+        {
+            WebClient client = new WebClient();
+            var google = client.DownloadStringTask("http://www.google.com");
+            yield return google;
+
+            throw new Exception("Testing an exception");
+        }
+
+		static void Main(string[] args)
+		{
+            var test1 = new AsyncTask<string>(Test1());
+            test1.Start();
+
+            var test2 = new AsyncTask<string>(Test2());
+            test2.Start();
+            
+            Console.WriteLine();
+
+            var test3 = new AsyncTask<string>(Test3());
+            test3.Start();
+
+            Task.WaitAll(test1, test2);
+
+            Console.WriteLine(test1.Result);
+            Console.WriteLine();
+            Console.WriteLine(test2.Result);
+            Console.WriteLine();
+            try
+            {
+                Console.WriteLine(test3.Result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Caught exception:");
+                Console.WriteLine(e);
+            }
+
+        }
+	}
+}
